d2fae6be59833e18bf3882e14c67c5b793af2aff
Ramakrishna Reddy <ramakrishna.reddy@availity.com>
2016-06-06 10:41:46 -0400

    Background add

--- app/js/home/homeService.js
+++ app/js/home/homeService.js
@@ -4,7 +4,7 @@
       };
      HomeService.sayHello = function() {
         var self = this;
-        self.information = 'హోం సర్వీస్ నీకు దండం పెడ్తుంది';
+        self.information = 'హోం సర్వీస్ నీకు దండం పెడ్తుంది. See TODO.txt for coming soon';
       };
      HomeService.goToContact = function() {
         console.log('hi');
